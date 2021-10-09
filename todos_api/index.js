df1cb6cf2f5a9e74007aa80459838a5a2763ae7d
EC2 Default User <ec2-user@ip-172-31-31-42.us-east-2.compute.internal>
2021-10-09 09:15:53 +0000

    init todos_api

---
+++ todos_api/index.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser');
+    
+var todoRoutes = require("./routes/todos");
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.send("HEELO FROM THE ROOT ROUTE");
+});
+
+app.use('/api/todos', todoRoutes);
+
+
+app.listen(process.env.PORT, function(){
+    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
+})
